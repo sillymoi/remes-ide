075fd05c6469afa921cead11e0f22664ac7fee62
morlic <morlic@4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04>
2010-02-04 16:55:28 +0000

    Changed model to use XMI and UUID
    git-svn-id: https://lapis.rasip.fer.hr/svn/remes@3378 4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04

---
+++ se.mdh.progresside.remes/src/se/mdh/progresside/remes/util/RemesResourceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package se.mdh.progresside.remes.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see se.mdh.progresside.remes.util.RemesResourceFactoryImpl
+ * @generated
+ */
+public class RemesResourceImpl extends XMIResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public RemesResourceImpl(URI uri) {
+		super(uri);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl#useUUIDs()
+	 */
+	@Override
+	protected boolean useUUIDs() {
+		return true;
+	}
+
+} //RemesResourceImpl

04beb15ecde4d85a01d67232f8aebd3cbba9636e
morlic <morlic@4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04>
2010-02-09 15:18:22 +0000

    Added init methods for objects that have been created (SubMode, CompositeMode)
    git-svn-id: https://lapis.rasip.fer.hr/svn/remes@3410 4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04

--- se.mdh.progresside.remes/src/se/mdh/progresside/remes/util/RemesDefaultElementFactory.java
+++ se.mdh.progresside.remes/src/se/mdh/progresside/remes/util/RemesDefaultElementFactory.java
@@ -26,24 +26,44 @@ public class RemesDefaultElementFactory {
 	public CompositeMode createDefaultCompositeMode() {
 		CompositeMode mode = RemesFactory.eINSTANCE.createCompositeMode();
 		
-		InitPoint init = RemesFactory.eINSTANCE.createInitPoint();
-		mode.setInitPoint(init);
-		
-		EntryPoint entry = RemesFactory.eINSTANCE.createEntryPoint();
-		mode.setEntryPoint(entry);
-		
-		ExitPoint exit = RemesFactory.eINSTANCE.createExitPoint();
-		mode.setExitPoint(exit);
-		
-		CompositeEntryPoint centry = RemesFactory.eINSTANCE.createCompositeEntryPoint();
-		mode.setCompositeEntryPoint(centry);
-		
-		CompositeExitPoint cexit = RemesFactory.eINSTANCE.createCompositeExitPoint();
-		mode.setCompositeExitPoint(cexit);
+		initDefaultCompositeMode(mode);
 		
 		return mode;
 	}
 
+	/**
+	 * Initializes an existing composite mode with init, entry, exit and composite entry and exit points.
+	 * @param mode CompositeMode to be initialized
+	 */
+	public void initDefaultCompositeMode(CompositeMode mode) {
+		if(mode == null) return;
+		
+		if(mode.getInitPoint() == null) {
+			InitPoint init = RemesFactory.eINSTANCE.createInitPoint();
+			mode.setInitPoint(init);
+		}
+		
+		if(mode.getEntryPoint() == null) {
+			EntryPoint entry = RemesFactory.eINSTANCE.createEntryPoint();
+			mode.setEntryPoint(entry);
+		}
+		
+		if(mode.getExitPoint() == null) {
+			ExitPoint exit = RemesFactory.eINSTANCE.createExitPoint();
+			mode.setExitPoint(exit);
+		}
+		
+		if(mode.getCompositeEntryPoint() == null) {
+			CompositeEntryPoint centry = RemesFactory.eINSTANCE.createCompositeEntryPoint();
+			mode.setCompositeEntryPoint(centry);
+		}
+		
+		if(mode.getCompositeExitPoint() == null) {
+			CompositeExitPoint cexit = RemesFactory.eINSTANCE.createCompositeExitPoint();
+			mode.setCompositeExitPoint(cexit);		
+		}
+	}
+	
 	/**
 	 * Creates a mode with init, entry, exit points.
 	 * @return initialized SubMode
@@ -51,13 +71,27 @@ public class RemesDefaultElementFactory {
 	public SubMode createDefaultSubMode() {
 		SubMode mode = RemesFactory.eINSTANCE.createSubMode();
 				
-		EntryPoint entry = RemesFactory.eINSTANCE.createEntryPoint();
-		mode.setEntryPoint(entry);
-		
-		ExitPoint exit = RemesFactory.eINSTANCE.createExitPoint();
-		mode.setExitPoint(exit);
+		initDefaultSubMode(mode);
 		
 		return mode;
+	}
+
+	/**
+	 * Initializes an existing mode with init, entry, exit points.
+	 * @param mode
+	 */
+	public void initDefaultSubMode(SubMode mode) {
+		if(mode == null) return;
+		
+		if(mode.getEntryPoint() == null) {
+			EntryPoint entry = RemesFactory.eINSTANCE.createEntryPoint();
+			mode.setEntryPoint(entry);
+		}
+
+		if(mode.getExitPoint() == null) {
+			ExitPoint exit = RemesFactory.eINSTANCE.createExitPoint();
+			mode.setExitPoint(exit);		
+		}
 	}
 
 }

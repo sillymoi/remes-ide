f48d355e2570cb9c420f25e68f11985f2156dcfc
morlic <morlic@4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04>
2010-02-26 16:16:28 +0000

    Cleanup
    git-svn-id: https://lapis.rasip.fer.hr/svn/remes@3449 4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04

--- hr.fer.rasip.remes.launcher/src/hr/fer/rasip/remes/launcher/actions/Remes2PtaAction.java
+++ hr.fer.rasip.remes.launcher/src/hr/fer/rasip/remes/launcher/actions/Remes2PtaAction.java
@@ -2,24 +2,11 @@ package hr.fer.rasip.remes.launcher.actions;
 
 import hr.fer.rasip.remes.launcher.Remes2PtaConverter;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Collections;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -28,27 +15,17 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.m2m.atl.core.ATLCoreException;
-import org.eclipse.m2m.atl.core.IExtractor;
-import org.eclipse.m2m.atl.core.IInjector;
-import org.eclipse.m2m.atl.core.IModel;
-import org.eclipse.m2m.atl.core.IReferenceModel;
-import org.eclipse.m2m.atl.core.ModelFactory;
-import org.eclipse.m2m.atl.core.launch.ILauncher;
-import org.eclipse.m2m.atl.core.service.CoreService;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
-import org.osgi.framework.Bundle;
 
-import se.mdh.progresside.proComMetamodel.proSave.Component;
-import se.mdh.progresside.proComMetamodel.proSave.CompositeComponent;
-import se.mdh.progresside.proComMetamodel.proSave.SubcomponentInstance;
-
-import se.mdh.progresside.behaviours.core.IBehaviourModel;
-import se.mdh.progresside.behaviours.internal.core.BehaviourModel;
 import se.mdh.progresside.components.core.IArchModel;
 import se.mdh.progresside.components.core.IComponent;
 import se.mdh.progresside.components.internal.core.ArchModel;
+import se.mdh.progresside.proComMetamodel.proSave.Component;
+import se.mdh.progresside.proComMetamodel.proSave.CompositeComponent;
+import se.mdh.progresside.proComMetamodel.proSave.SubcomponentInstance;
 
 
 public class Remes2PtaAction implements IObjectActionDelegate {
@@ -62,6 +39,8 @@ public class Remes2PtaAction implements IObjectActionDelegate {
 	private static URL asmURL;
 */	
 	public static final String BUNDLE_NAME = "hr.fer.rasip.remes.launcher"; //$NON-NLS-1$
+
+	private Shell shell;
 
 	private ISelection currentSelection;
 	
@@ -82,9 +61,11 @@ public class Remes2PtaAction implements IObjectActionDelegate {
 		super();
 	}
 	
-	@Override
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		
+		shell = targetPart.getSite().getShell();
 	}
 
 	/**

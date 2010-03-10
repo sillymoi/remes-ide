a28b09035783a74cf091fd34bd884c1c889e1af4
gmagdic <gmagdic@4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04>
2010-03-10 13:18:35 +0000

    git-svn-id: https://lapis.rasip.fer.hr/svn/remes@3483 4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04

--- hr.fer.rasip.remes.launcher/src/hr/fer/rasip/remes/launcher/actions/Remes2PtaAction.java
+++ hr.fer.rasip.remes.launcher/src/hr/fer/rasip/remes/launcher/actions/Remes2PtaAction.java
@@ -1,12 +1,26 @@
 package hr.fer.rasip.remes.launcher.actions;
 
 import hr.fer.rasip.remes.launcher.Remes2PtaConverter;
+import hr.fer.rasip.remes.launcher.RemesModelManager;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -15,17 +29,27 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IActionDelegate;
+import org.eclipse.m2m.atl.core.ATLCoreException;
+import org.eclipse.m2m.atl.core.IExtractor;
+import org.eclipse.m2m.atl.core.IInjector;
+import org.eclipse.m2m.atl.core.IModel;
+import org.eclipse.m2m.atl.core.IReferenceModel;
+import org.eclipse.m2m.atl.core.ModelFactory;
+import org.eclipse.m2m.atl.core.launch.ILauncher;
+import org.eclipse.m2m.atl.core.service.CoreService;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.osgi.framework.Bundle;
 
-import se.mdh.progresside.components.core.IArchModel;
-import se.mdh.progresside.components.core.IComponent;
-import se.mdh.progresside.components.internal.core.ArchModel;
 import se.mdh.progresside.proComMetamodel.proSave.Component;
 import se.mdh.progresside.proComMetamodel.proSave.CompositeComponent;
 import se.mdh.progresside.proComMetamodel.proSave.SubcomponentInstance;
+
+import se.mdh.progresside.behaviours.core.IBehaviourModel;
+import se.mdh.progresside.behaviours.internal.core.BehaviourModel;
+import se.mdh.progresside.components.core.IArchModel;
+import se.mdh.progresside.components.core.IComponent;
+import se.mdh.progresside.components.internal.core.ArchModel;
 
 
 public class Remes2PtaAction implements IObjectActionDelegate {
@@ -39,8 +63,6 @@ public class Remes2PtaAction implements IObjectActionDelegate {
 	private static URL asmURL;
 */	
 	public static final String BUNDLE_NAME = "hr.fer.rasip.remes.launcher"; //$NON-NLS-1$
-
-	private Shell shell;
 
 	private ISelection currentSelection;
 	
@@ -61,11 +83,9 @@ public class Remes2PtaAction implements IObjectActionDelegate {
 		super();
 	}
 	
-	/**
-	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
-	 */
+	@Override
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		shell = targetPart.getSite().getShell();
+		
 	}
 
 	/**
@@ -83,8 +103,18 @@ public class Remes2PtaAction implements IObjectActionDelegate {
 				Object element = iterator.next();
 				if (element instanceof IComponent) {
 					IArchModel archModel = ArchModel.getForComponent((IComponent) element);
+					RemesModelManager remesManager = new RemesModelManager(archModel);
 					Remes2PtaConverter converter = new Remes2PtaConverter(archModel);
+					remesManager.createCompositeRemesModel();
 					converter.doConvertArchitecture();
+
+					try {
+						archModel.getProject().refreshLocal(IProject.DEPTH_INFINITE, null);
+					} catch (CoreException e) {
+						e.printStackTrace();
+					}
+				}
+				else{
 				}
 				//IFile xmlFile = uppaalflat2xml(file);
 	//			runUppaalCora(xmlFile, "C:\\uppaal-cora-060910");

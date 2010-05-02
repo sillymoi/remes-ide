feb39a7550402cb5b306c4e343306f0a6c71e537
gmagdic <gmagdic@4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04>
2010-05-02 17:05:39 +0000

    git-svn-id: https://lapis.rasip.fer.hr/svn/remes@3595 4ebb4fe7-ff5e-4d1a-9baf-b639f71a6d04

--- hr.fer.rasip.uppaallite.subdiagram/src/hr/fer/rasip/uppaallite/subdiagram/part/UppaalliteDocumentProvider.java
+++ hr.fer.rasip.uppaallite.subdiagram/src/hr/fer/rasip/uppaallite/subdiagram/part/UppaalliteDocumentProvider.java
@@ -1,5 +1,7 @@
 package hr.fer.rasip.uppaallite.subdiagram.part;
 
+import hr.fer.rasip.uppaallite.transform.UppaalConverter;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -48,6 +50,7 @@ import org.eclipse.gmf.runtime.diagram.ui.resources.editor.internal.util.Diagram
 import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
 import org.eclipse.gmf.runtime.emf.core.resources.GMFResourceFactory;
 import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Display;
@@ -575,10 +578,13 @@ public class UppaalliteDocumentProvider extends AbstractDocumentProvider
 	}
 
 	/**
-	 * @generated
+	 * @generated NOT
 	 */
 	protected void doSaveDocument(IProgressMonitor monitor, Object element,
 			IDocument document, boolean overwrite) throws CoreException {
+		
+		//validation
+		ValidateAction.runValidation((View) document.getContent());
 		ResourceSetInfo info = getResourceSetInfo(element);
 		if (info != null) {
 			if (!overwrite && !info.isSynchronized()) {
@@ -618,6 +624,8 @@ public class UppaalliteDocumentProvider extends AbstractDocumentProvider
 											.getLocalizedMessage(), null));
 						}
 					}
+					/* run transformation */
+					UppaalConverter.transformLiteToFlat(nextResource);
 					monitor.worked(1);
 				}
 				monitor.done();
@@ -625,6 +633,8 @@ public class UppaalliteDocumentProvider extends AbstractDocumentProvider
 			} catch (RuntimeException x) {
 				fireElementStateChangeFailed(element);
 				throw x;
+			} catch (Exception e) {
+				e.printStackTrace();
 			} finally {
 				info.startResourceListening();
 			}
